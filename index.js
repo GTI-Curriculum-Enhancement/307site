966d577958ed78d43c13a1cc77225aef53befc04
Sombody101 <S101@git.dev>
2024-01-22 21:40:31 -0700

    Added welcome page and styles

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const DEBUG = true
+const DEBUG = true;
+contentLoaded = false;
+
+async function showContent(force) {
+    const overlayDiv = document.getElementById("overlayDiv");
+
+    if (overlayDiv) {
+        if (force) {
+            window.scrollTo(0, 0);
+            overlayDiv.style.opacity = "0";
+            overlayDiv.remove();
+        } else
+            setTimeout(() => {
+                if (contentLoaded) {
+                    window.scrollTo(0, 0);
+                    overlayDiv.style.opacity = "0";
+
+                    setTimeout(() => overlayDiv.remove(), 1000);
+                } else showContent();
+            }, 100);
+    }
+}
+
+function main() {
+    showContent();
+
+    contentLoaded = true
+}
+
+main();
